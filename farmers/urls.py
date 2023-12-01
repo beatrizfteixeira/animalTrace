3fc8cd6e837f2c28869b5864730bb40c2545a679
beatrizfteixeira <biaft03@protonmail.com>
2023-11-30 21:50:45 -0300

    bootstrap5 adicionado nas paginas de mapa e login

--- farmers/urls.py
+++ farmers/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('mapa', views.mapa, name='ver google maps')
+    path('mapa', views.mapa, name='mapa'),
+    path('', views.login , name='login'),
+    path('signup', views.signup, name='signup')
 ]
--- farmers/views.py
+++ farmers/views.py
@@ -6,3 +6,11 @@ def mapa(request):
         'GOOGLE_MAPS_API_KEY': settings.GOOGLE_MAPS_API_KEY
     }
     return render(request, 'mapa.html', context)
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
