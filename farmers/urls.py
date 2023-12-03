6b14648ec60bfb80ac644d35b7f9bf46f46cb1e8
jh-ennifer <jhenniferrodrigues200116@gmail.com>
2023-12-03 14:42:08 -0300

    criação do caminho para o post das coordenadas

--- farmers/urls.py
+++ farmers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import recebe_coordenadas
 
 urlpatterns = [
     path('mapa', views.mapa, name='mapa'),
     path('', views.login , name='login'),
-    path('signup', views.signup, name='signup')
+    path('signup', views.signup, name='signup'),
+    path('coordenadas/', recebe_coordenadas, name='coordenadas')
 ]
